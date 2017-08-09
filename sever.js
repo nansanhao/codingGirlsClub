711373c32842ad794f86fd2663d3483b41982228
yuehuafei <1490165179@qq.com>
2017-08-09 15:25:14 +0800

    add sever

--- sever.js
+++ sever.js
@@ -1,4 +1,45 @@
 'use strict';
 let express =require('express');
 let orm=require('orm');
+let bodyparser=require("body-parser");
 let app = express();
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(express.static('public'));
+app.use(orm.express("sqlite:public/CodingGirlsClub.db",{
+    define:function (db,models,next) {
+        models.Position=db.define("POSITIONS",{
+            id:{type:'number'},
+            title:{type:'text'},
+            company:{type:'text'},
+            description:{type:'text'},
+            applyMethod:{type:'text'},
+            expiryDate:{type:'text'},
+            category:{type:'text'},
+            jobType:{type:'text'},
+            tags:{type:'text'},
+            city:{type:'text'},
+            country:{type:'text'},
+            condition:{type:'text'},
+            owner:{type:'number'},
+            publishTime:{type:'text'},
+            invalidTime:{type:'text'}
+        });
+        models.User=db.define("USERS",{
+            id:{type:'number'},
+            usrName:{type:'text'},
+            usrPassword:{type:'text'},
+            usrEmail:{type:'text'},
+            usrCompanyName:{type:'text'},
+            usrCompanyAddress:{type:'text'},
+            usrCompanyProfession:{type:'text'}
+        });
+        next();
+    }
+}));
+//此处写API
+var server = app.listen(8081, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("应用实例，访问地址为 http://%s:%s", host, port)
+
+});
