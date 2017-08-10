a85622b216bc07811ba8268e7c9590346063097c
ShiJie Kang <https://github.com/kangkang1234/17.07.11.git>
2017-08-10 14:00:11 +0800

    api3,4

--- sever.js
+++ sever.js
@@ -1,52 +1,102 @@
 'use strict';
-const express =require('express');
-const orm=require('orm');
-const app = express();
-const bodyparser = require('body-parser');
-app.use(bodyparser.urlencoded({ extended: true }));
+let express =require('express');
+let orm=require('orm');
+let bodyparser=require("body-parser");
+let app = express();
+app.use(bodyparser.urlencoded({extended:true}));
 app.use(express.static('public'));
-
-app.use(orm.express("sqlite:/home/shijie/桌面/girlclub/codingGirlsClub/CodingGirlsClub.db", {
-    define: function (db, models, next) {
-        models.user = db.define("USERS", {
-            id: {type: 'number'},
-            usrName : {type: 'text'},
-            usrPassword : {type: 'text'},
-            usrEmail : {type: 'text'},
-            usrCompanyName : {type: 'text'},
-            usrCompanyAddress : {type: 'text'},
-            usrCompanyProfession: {type: 'text'}
+app.use(orm.express("sqlite:public/CodingGirlsClub.db",{
+    define:function (db,models,next) {
+        models.Position=db.define("POSITIONS",{
+            id:{type:'number'},
+            title:{type:'text'},
+            company:{type:'text'},
+            description:{type:'text'},
+            applyMethod:{type:'text'},
+            expiryDate:{type:'text'},
+            category:{type:'text'},
+            jobType:{type:'text'},
+            tags:{type:'text'},
+            city:{type:'text'},
+            country:{type:'text'},
+            condition:{type:'text'},
+            owner:{type:'text'},
+            publishTime:{type:'text'},
+            invalidTime:{type:'text'}
         });
-        models.positions=db.define("POSITIONS",{
-            id:  {type: 'number'},
-            title:  {type: 'text'},
-            company:  {type: 'text'},
-            description:  {type: 'text'},
-            applyMethod : {type: 'text'},
-            expiryDate : {type: 'text'},
-            category : {type: 'text'},
-            jobType : {type: 'text'},
-            tags : {type: 'text'},
-            city : {type: 'text'},
-            country: {type: 'text'},
-            condition:  {type: 'text'},
-            owner: {type: 'text'},
-            publishTime: {type: 'text'},
-            invalidTime: {type: 'text'}
+        models.User=db.define("USERS",{
+            id:{type:'number'},
+            usrName:{type:'text'},
+            usrPassword:{type:'text'},
+            usrEmail:{type:'text'},
+            usrCompanyName:{type:'text'},
+            usrCompanyAddress:{type:'text'},
+            usrCompanyProfession:{type:'text'}
         });
         next();
     }
 }));
+app.get('/',function (req,res) {
+    res.sendFile(__dirname+"/public/html/home.html")
+})
+app.put('/usrs/:emailId/positions/:id',function (req,res) {
+    //检测数据是否取到
+    let email=req.params.emailId;
+    let positionId=req.params.id;
+    console.log(email);
+    console.log(positionId);
+    req.models.Position.find({owner:email,id:positionId},function (err,position){
+        position[0].remove(function (err) {
+            if(err){
+                console.log("错误");
+            }
+        });
+        var newRecord={};
+        newRecord.id=req.body.id;
+        newRecord.title=req.body.title;
+        newRecord.company=req.body.company;
+        newRecord.description=req.body.description;
+        newRecord.applyMethod=req.body.applyMethod;
+        newRecord.expiryDate=req.body.expiryDate;
+        newRecord.category=req.body.category;
+        newRecord.jobType=req.body.jobType;
+        newRecord.tags=req.body.tags;
+        newRecord.city=req.body.city;
+        newRecord.country=req.body.country;
+        newRecord.condition=req.body.condition;
+        newRecord.owner=req.body.owner;
+        newRecord.publishTime=req.body.publishTime;
+        newRecord.invalidTime=req.body.invalidTime;
+        req.models.Position.create(newRecord,function(err,result){
+            console.log(JSON.stringify(newRecord));
+            console.log("修改成功");
+        });
+        //测试用例
+        // req.models.Position.find({id:1001},function (err,ans) {
+        //     console.log(JSON.stringify(ans));
+        // })
+    });
+});
+app.get('/usrs/:emailId/positions/:id',function (req,res) {
+    let email=req.params.emailId;
+    let positionId=req.params.id;
+    console.log(email);
+    console.log(positionId);
+    req.models.Position.find({owner:email,id:positionId},function (err,position) {
+        console.log(JSON.stringify(position));
+        res.json(position);
+    })
+})
 
 app.get("/positions/id",function (req,res) {
-    var getInfo = req.query.Id;
+    var getInfo = req.query.Id;  //Id
     if(getInfo===''){
-        req.models.positions.find(null,function (err,position) {
+        req.models.Position.find(null,function (err,position) {
             res.json(position);
         })
     }
     else {
-        req.models.positions.find({Id:getInfo},function (err,position) {
+        req.models.Position.find({Id:getInfo},function (err,position) {
             res.json(position);
         })
     }
@@ -55,17 +105,19 @@ app.get("/positions/id",function (req,res) {
 app.get("/users/emailId",function (req,res) {
     var getInfo = req.query.emailId;
     if(getInfo===''){
-        req.models.user.find(null,function (err,usr) {
+        req.models.User.find(null,function (err,usr) {
             res.json(usr);
         })
     }
     else{
-        req.models.user.find({usrEmail:getInfo},function (err,usr) {
+        req.models.User.find({usrEmail:getInfo},function (err,usr) {
             res.json(usr);
         })
     }
 });
 
-app.listen(3000,function () {
-    console.log("This");
+var server = app.listen(8081, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("应用实例，访问地址为 http://%s:%s", host, port)
 });
