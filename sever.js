2c5f272fe0e7cbee579f3edf673bf95c1c498211
yuehuafei <1490165179@qq.com>
2017-08-10 10:54:52 +0800

    api 10,11

--- sever.js
+++ sever.js
@@ -20,7 +20,7 @@ app.use(orm.express("sqlite:public/CodingGirlsClub.db",{
             city:{type:'text'},
             country:{type:'text'},
             condition:{type:'text'},
-            owner:{type:'number'},
+            owner:{type:'text'},
             publishTime:{type:'text'},
             invalidTime:{type:'text'}
         });
@@ -36,7 +36,74 @@ app.use(orm.express("sqlite:public/CodingGirlsClub.db",{
         next();
     }
 }));
-//此处写API
+
+// app.get('/',function(req,res){
+//     let count=0;
+//     req.models.Position.count(null , function(err, positionCount) {
+//        count=positionCount;
+//        console.log(positionCount);
+//     });
+// })
+// 9.POST 一个用户新建一个职位。（接收一个职位JOSN对象）
+// app.post("/usrs/:emailId/positions",fuction{
+//     res.send("positionId")
+// })
+// 10.PUT 一个用户修改‪一个职位的信息（接收一个职位JOSN对象）
+// app.put("/usrs/:emailId/positions/:id",fuction{
+//     res.send("ok")
+// })
+// 11.GET 一个用户获得‪一个职位的信息（返回一个职位JOSN对象）
+// app.get("/usrs/:emailId/positions/:id",fuction{
+//     res.send(position)
+// })
+app.put('/usrs/:emailId/positions/:id',function (req,res) {
+    //检测数据是否取到
+    let email=req.params.emailId;
+    let positionId=req.params.id;
+    console.log(email);
+    console.log(positionId);
+    req.models.Position.find({owner:email,id:positionId},function (err,position){
+        position[0].remove(function (err) {
+            if(err){
+                console.log("错误");
+            }
+        });
+        var newRecord={};
+        newRecord.id=req.body.id;
+        newRecord.title=req.body.title;
+        newRecord.company=req.body.company;
+        newRecord.description=req.body.description;
+        newRecord.applyMethod=req.body.applyMethod;
+        newRecord.expiryDate=req.body.expiryDate;
+        newRecord.category=req.body.category;
+        newRecord.jobType=req.body.jobType;
+        newRecord.tags=req.body.tags;
+        newRecord.city=req.body.city;
+        newRecord.country=req.body.country;
+        newRecord.condition=req.body.condition;
+        newRecord.owner=req.body.owner;
+        newRecord.publishTime=req.body.publishTime;
+        newRecord.invalidTime=req.body.invalidTime;
+        req.models.Position.create(newRecord,function(err,result){
+            console.log(JSON.stringify(newRecord));
+            console.log("修改成功");
+        });
+        //测试用例
+        // req.models.Position.find({id:1001},function (err,ans) {
+        //     console.log(JSON.stringify(ans));
+        // })
+    });
+});
+app.get('/usrs/:emailId/positions/:id',function (req,res) {
+    let email=req.params.emailId;
+    let positionId=req.params.id;
+    console.log(email);
+    console.log(positionId);
+    req.models.Position.find({owner:email,id:positionId},function (err,position) {
+        console.log(JSON.stringify(position));
+        res.json(position);
+    })
+})
 var server = app.listen(8081, function () {
     var host = server.address().address;
     var port = server.address().port;
