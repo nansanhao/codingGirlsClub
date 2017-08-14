efdfc647016d3d7856854285be7eadeedc999eb5
Anchoret13 <vergil1133@live.com>
2017-08-14 11:29:21 +0800

    Login System
    Someone help me to have a test?????

---
+++ config/database.js
@@ -0,0 +1,12 @@
+// config/database.js
+/*
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'password'
+    },
+	'database': 'my_schema',
+    'users_table': 'users'
+};
+*/
